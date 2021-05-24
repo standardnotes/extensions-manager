dcaf8bbbc302c86e4bcd3c11a3d956127b2bee07
Johnny A <5891646+johnny243@users.noreply.github.com>
2021-05-24 12:54:22 -0400

    enhancements (#25)
    * fix: lint Advanced.js file
    
    * fix: clear URL field after repo/package is added
    
    * fix: lint RepoView.js file
    
    * fix: remove duplicate confirm when deleting a repository
    
    * fix: update engines.node
    
    * chore: increment version
    
    * chore: build
    
    Co-authored-by: Johnny Almonte <johnny243@users.noreply.github.com>

--- app/components/RepoView.js
+++ app/components/RepoView.js
@@ -1,77 +1,84 @@
-import React from 'react';
+import React from "react";
 import RepoController from "../lib/RepoController.js";
 import BridgeManager from "../lib/BridgeManager.js";
 import PackageView from "./PackageView";
 
 export default class RepoView extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = {packages: []};
+    this.state = { packages: [] };
 
     this.needsUpdateComponents = true;
 
-    this.repoController = new RepoController({repo: props.repo});
+    this.repoController = new RepoController({ repo: props.repo });
     this.refreshRepo();
 
     this.updateObserver = BridgeManager.get().addUpdateObserver(() => {
       this.receivedBridgeItems = true;
-      if(this.needsUpdateComponents && this.state.packages.length > 0) {
+      if (this.needsUpdateComponents && this.state.packages.length > 0) {
         this.updateComponentsWithNewPackageInfo();
       }
       this.reload();
-    })
+    });
   }
 
   refreshRepo() {
     BridgeManager.get().notifyEvent(BridgeManager.EventDownloadingPackages);
     this.repoController.getPackages((response) => {
-      BridgeManager.get().notifyEvent(BridgeManager.EventDoneDownloadingPackages);
-      if(response) {
+      BridgeManager.get().notifyEvent(
+        BridgeManager.EventDoneDownloadingPackages
+      );
+      if (response) {
         var packages = response.packages;
         var valid_until = new Date(response.valid_until);
-        BridgeManager.get().notifyEvent(BridgeManager.EventUpdatedValidUntil, {valid_until});
+        BridgeManager.get().notifyEvent(BridgeManager.EventUpdatedValidUntil, {
+          valid_until,
+        });
         BridgeManager.get().registerPackages(packages);
-        this.setState({packages: packages || []});
-        if(this.receivedBridgeItems) {
+        this.setState({ packages: packages || [] });
+        if (this.receivedBridgeItems) {
           this.updateComponentsWithNewPackageInfo();
         }
       }
-    })
+    });
   }
 
   updateComponentsWithNewPackageInfo() {
     this.needsUpdateComponents = false;
     // Update expiration dates for packages
-    var needingSave = []
-    for(let packageInfo of this.state.packages) {
+    var needingSave = [];
+    for (let packageInfo of this.state.packages) {
       let installed = BridgeManager.get().itemForPackage(packageInfo);
-      if(installed) {
+      if (installed) {
         var needsSave = false;
         let validUntil = new Date(packageInfo.valid_until);
         // .getTime() must be used to compare dates
-        if(packageInfo.valid_until && (!installed.content.valid_until || (new Date(installed.content.valid_until).getTime() !== validUntil.getTime()))) {
+        if (
+          packageInfo.valid_until &&
+          (!installed.content.valid_until ||
+            new Date(installed.content.valid_until).getTime() !==
+              validUntil.getTime())
+        ) {
           installed.content.valid_until = validUntil;
           needsSave = true;
         }
-
 
         /*
         As part of the below condition, we used to also have if(JSON.stringify(installed.content.package_info) !== JSON.stringify(packageInfo))
         to copy over package info. However, if the repo updates a version, then the installed component's version would also update without
         */
-        if(!installed.content.package_info) {
+        if (!installed.content.package_info) {
           installed.content.package_info = packageInfo;
           needsSave = true;
         }
 
-        if(needsSave) {
+        if (needsSave) {
           needingSave.push(installed);
         }
       }
     }
 
-    if(needingSave.length > 0) {
+    if (needingSave.length > 0) {
       BridgeManager.get().saveItems(needingSave);
     } else {
       BridgeManager.get().notifyObserversOfUpdate();
@@ -87,43 +94,49 @@ export default class RepoView extends React.Component {
   }
 
   toggleOptions = () => {
-    this.setState({showOptions: !this.state.showOptions});
-  }
+    this.setState({ showOptions: !this.state.showOptions });
+  };
 
   deleteRepo = () => {
-    if(confirm("Are you sure you want to delete this repository?")) {
-      BridgeManager.get().uninstallRepo(this.props.repo);
-    }
-  }
+    BridgeManager.get().uninstallRepo(this.props.repo);
+  };
 
   render() {
     return (
       <div className="sk-panel-section">
-
         <div className="sk-panel-section">
           <div className="sk-panel-row">
-            <div className="sk-panel-section-title info sk-bold">Repository</div>
+            <div className="sk-panel-section-title info sk-bold">
+              Repository
+            </div>
           </div>
 
-          <a onClick={this.toggleOptions} className="info">Options</a>
-          {this.state.showOptions &&
+          <a onClick={this.toggleOptions} className="info">
+            Options
+          </a>
+          {this.state.showOptions && (
             <div className="sk-panel-row">
-              <a onClick={this.deleteRepo} className="danger sk-a">Delete Repository</a>
+              <a onClick={this.deleteRepo} className="danger sk-a">
+                Delete Repository
+              </a>
             </div>
-          }
+          )}
         </div>
 
         <div className="sk-panel-row">
           <div className="packages sk-panel-table">
-            {this.state.packages.map((p, index) =>
+            {this.state.packages.map((p, index) => (
               <div className="package sk-panel-table-item">
-                <PackageView repo={this.props.repo} key={p.identifier} packageInfo={p} />
+                <PackageView
+                  repo={this.props.repo}
+                  key={p.identifier}
+                  packageInfo={p}
+                />
               </div>
-            )}
+            ))}
           </div>
         </div>
       </div>
-    )
+    );
   }
-
 }
